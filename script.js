f7658acc8c975ed283cae5e58aefbda64bb307b1
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-03 09:21:01 -0500

    Create operation functions and operator.

---
+++ script.js
@@ -0,0 +1,42 @@
+//scripts for calculator application
+
+function add(a, b) {
+
+    return a + b;
+}
+
+function subtract(a, b) {
+
+    return a - b;
+}
+
+function multiply(a, b) {
+
+    return a * b;
+}
+
+function divide(a, b) {
+
+    return a/b;
+}
+
+// console.log(add(1,2));
+// console.log(subtract(1,-2));
+// console.log(multiply(1,2));
+// console.log(divide(1,-2));
+
+//operate calls any of the functions above
+
+function operator(operation, a, b) {
+
+    if(!(typeof operation === 'function')) return "Invalid operation";
+    if(!(typeof a === 'number') || !(typeof b === 'number')) return "Non-numeric input";
+
+    return operation(a, b);
+}
+
+// console.log(operator(add, 1, 2));
+// console.log(typeof add);
+// console.log(operator(subtract, 2, '-1'));
+// console.log(operator("fun", 2, 1));
+
